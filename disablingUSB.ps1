b13f27d9626000c9b8732d48b5a30d2150d30b8f
Aryan Kumar Singh <41736750+vashthestampede95@users.noreply.github.com>
2018-11-05 15:46:20 +0530

    Create disablingUSB.ps1

---
+++ disablingUSB.ps1
@@ -0,0 +1,9 @@
+Get-ItemProperty  "HKLM:\SYSTEM\CurrentControlSet\services\USBSTOR" -name start
+
+
+PSPath       : Microsoft.PowerShell.Core\Registry::HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\services\USBSTOR
+PSParentPath : Microsoft.PowerShell.Core\Registry::HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\services
+PSChildName  : USBSTOR
+PSDrive      : HKLM
+PSProvider   : Microsoft.PowerShell.Core\Registry
+Start        : 4
