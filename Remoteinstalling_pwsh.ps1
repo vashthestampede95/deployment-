484a3a364ff665f51e6a7dfc2b5c9d35274d5375
Aryan Kumar Singh <41736750+vashthestampede95@users.noreply.github.com>
2018-11-26 09:44:52 +0530

    Update Remoteinstalling_pwsh.ps1

--- Remoteinstalling_pwsh.ps1
+++ Remoteinstalling_pwsh.ps1
@@ -36,3 +36,16 @@ if(test-Connection -Cn $computer -quiet)
         }
   }      
   }
+  function Get-TimeStamp{
+  return"[{0:MM/dd/yy} {0:HH:mm:ss}]" -f (Get-Date)
+}
+unction main(){
+  #make sure the account key 
+  Debug-Print("Checking for Account Key")
+  if ($AccountKey -eq "__ACCOUNT_KEY__") {
+    Write-Warning "$(Get-TimeStamp) AccountKey not set, exiting script!"
+    exit 1
+} elseif ($AccountKey.length -ne 32) {
+    Write-Warning "$(Get-TimeStamp) Invalid AccountKey specified, exiting script!"
+    exit 1
+}
